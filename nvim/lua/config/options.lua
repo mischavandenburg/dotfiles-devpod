90b8c3ae15610f92eb7f6b8b863ce082ce05dd2a
Mischa van den Burg <mischavandenburg@gmail.com>
2024-06-19 17:02:58 +0000

    feat: move mdpreview to opts

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -28,3 +28,9 @@ vim.g.lazygit_config = false
 -- vim.g.mkdp_port = 30138
 -- vim.g.mkdp_browser = "none"
 -- vim.g.mkdp_echo_preview_url = 1
+
+vim.g.mkdp_open_to_the_world = 1
+vim.g.mkdp_open_ip = "127.0.0.1"
+vim.g.mkdp_port = 30108
+vim.g.mkdp_browser = "none"
+vim.g.mkdp_echo_preview_url = 1
