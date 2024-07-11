fde304e7a5527c084d8e6939d30b3241304949ef
Mischa van den Burg <mischavandenburg@gmail.com>
2024-07-11 05:18:58 +0000

    feat: disable 80 character line length

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# dotfiles-devpod
+# Devpod Dotfiles
+
 Slimmed-down dotfiles to use in Devpods
+
+## Markdown
+
+Currently not using the 80 character line width. It messes with X threads and LinkedIn posts.
+
+To disable the markdownlint rule, create .markdownlint.yaml containing `MD013: false`.
--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -3,12 +3,12 @@
 -- Add any additional autocmds here
 
 -- Set textwidth to 80 and automatic line breaks for markdown files
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "gitcommit", "markdown", "pandoc" },
-  callback = function()
-    require("cmp").setup({ enabled = false })
-    vim.opt_local.textwidth = 80
-    -- vim.opt_local.formatoptions:append("a")
-    vim.opt_local.colorcolumn = "80"
-  end,
-})
+-- vim.api.nvim_create_autocmd("FileType", {
+--   pattern = { "gitcommit", "markdown", "pandoc" },
+--   callback = function()
+--     require("cmp").setup({ enabled = false })
+--     vim.opt_local.textwidth = 80
+--     -- vim.opt_local.formatoptions:append("a")
+--     vim.opt_local.colorcolumn = "80"
+--   end,
+-- })
